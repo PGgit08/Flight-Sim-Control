d504559906dcbcfdae374670d6a99a75002d0f00
PGgit08 <60079012+PGgit08@users.noreply.github.com>
2021-11-02 17:11:42 -0400

    successful throttle control

--- PythonControl/PythonControl.py
+++ PythonControl/PythonControl.py
@@ -1,9 +1,16 @@
 from serial import Serial
 import keyboard
+import pyautogui
 
 arduino = Serial(port='COM5', baudrate=9600, timeout=.1)
 throttle_pos = 0
 init_pos = False
+
+# Divisor for throttle
+DIVISOR = 113
+
+# GEOFS plane speed
+speed = 0
 
 def read():
     data = arduino.readline()
@@ -17,7 +24,7 @@ def read():
         return False
 
 while True:
-    if keyboard.is_pressed('q'):
+    if keyboard.is_pressed('space'):
         arduino.close()
         break
     
@@ -25,15 +32,22 @@ while True:
 
     # make sure that something was successfully recieved
     if recieved:
-
         if not init_pos:
-            print('not init pos')
             throttle_pos = recieved
             init_pos = True
+            speed = throttle_pos // DIVISOR
+            print('init pos set')
         
         # if throttle pos changed(small range here)
         if not (throttle_pos + 2 >= recieved >= throttle_pos - 2):
-            print(recieved)
             throttle_pos = recieved
+            old_speed = speed
+            speed = throttle_pos // DIVISOR
+            print(speed)
+
+            # if change in speed
+            if speed != old_speed:
+                pyautogui.press(str(speed))
+            
             
     
