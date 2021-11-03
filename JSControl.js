39f7018ea777b7e97234a4384fc35efbb4d55920
PGgit08 <60079012+PGgit08@users.noreply.github.com>
2021-11-02 22:34:11 -0400

    thinking...

---
+++ JSControl.js
@@ -0,0 +1,8 @@
+/**
+ * Possible Javascript extension for flight sim.
+ * Communicates with geofs.
+ * 
+ * SOME GEOFS DATA
+ * geofs.airplane.instance: get info on current airplane instance
+ * controls: control data on current airplane
+ */
